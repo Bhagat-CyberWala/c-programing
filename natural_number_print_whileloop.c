cc0b4a4ef3506c0afdc7ae1cc5b67ecef2229c52
vishal746 <bhagat8920@gmail.com>
2022-09-18 22:24:31 +0530

    Print natural numbber using while loop

---
+++ natural_number_print_whileloop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i=0,n;
+    printf("Enter a number : ");
+     scanf("%d",&n);
+    
+    while (i<=n)
+    {
+        /* code */
+    printf("%d\n",i);
+    i++;
+        
+    }
+
+}
